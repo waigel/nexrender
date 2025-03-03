7b19164bbc6bb35bb22412fc7b3f16120bcff37d
waigel <johannes@waigel.me>
2025-03-03 12:00:31 +0100

    add extensions to binary build

--- misc/prelink.js
+++ misc/prelink.js
@@ -5,8 +5,12 @@ const {execSync} = require('child_process')
 console.log(`> linking all peer dependencies of ${pkg.name} as children to be prebuilt into binary...`)
 
 Object.keys(pkg.peerDependencies).map(dep => {
-    if (dep.indexOf('nexrender') !== -1) {
+    if (dep.indexOf('@nexrender/') !== -1) {
         const command = `npm link --no-package-lock --legacy-peer-deps ../${dep.replace('@nexrender/', 'nexrender-')}`
+        console.log(`executing > ${command}`)
+        execSync(command)
+    }else {
+        const command = `npm link --no-package-lock --legacy-peer-deps ../../extensions/${dep}`
         console.log(`executing > ${command}`)
         execSync(command)
     }
